fe52a0f9a6671fb049aa31e7b6c8b266f9a3040e
Piotr Ostrowski <piotr.ostrowski02@wp.pl>
2025-11-15 10:37:39 +0100

    fix

--- LAB1/dane_do_zadań/studenci.pl
+++ LAB1/dane_do_zadań/studenci.pl
@@ -4,9 +4,9 @@ student(ola, ekonomia, 3).
 student(ania, ekonomia, 3).
 student(pawel, informatyka, 4).
 
-prowadzi(kowalski, programowanie).
-prowadzi(nowak, bazy_danych).
-prowadzi(kaminska, ekonometria).
+prowadzi(kowalski, informatyka).
+prowadzi(nowak, ekonomia).
+prowadzi(kaminska, fizjoterapia).
 
 % Dwie osoby uczą się razem, jeśli są na tym samym kierunku i semestrze,
 % ale nie są tą samą osobą.
@@ -15,12 +15,6 @@ uczy_sie_z(X, Y) :-
     student(Y, Kierunek, Semestr),
     X \= Y.
 
-
-# 1. Reguła `uczy_sie_z/2` wykorzystuje unifikację — Prolog dopasowuje zmienne X i Y
-#    do wszystkich studentów o tym samym kierunku i semestrze.
-
-# 2. `X \= Y` wymusza, że porównywane osoby są różne, aby uniknąć sytuacji, 
-#    w której program uzna, że ktoś „uczy się sam ze sobą”.
-
-# 3. Unifikacja działa tak, że Prolog sprawdza wszystkie kombinacje faktów `student/3`
-#    i znajduje te, które spełniają oba warunki (ten sam kierunek i semestr).
+prowadzi_dla_studenta(Pracownik, Student) :-
+    student(Student, Kierunek, _),
+    prowadzi(Pracownik, Kierunek).
